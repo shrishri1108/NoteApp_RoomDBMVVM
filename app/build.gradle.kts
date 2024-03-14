fcc4d6a5be42b8463d3157a04dd482c2dcfc92ac
shrishri1108 <mypracticeemails@gmail.com>
2024-03-14 23:27:44 +0530

    required dependencies are listed here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id ("kotlin-kapt")
 }
 
 android {
@@ -50,4 +51,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val lifecycle_version = "2.2.0"
+    val room_version = "2.2.5"
+
+    implementation ("com.google.android.material:material:1.2.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycle_version")
+
+// Annotation processor
+    kapt ("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+
+// Android Room
+    implementation ("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
 }
