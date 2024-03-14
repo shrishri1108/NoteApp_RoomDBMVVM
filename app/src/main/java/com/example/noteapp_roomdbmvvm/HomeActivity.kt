ad3a2e8b2f876039ffd0ecefa04d590c3c70c37e
shrishri1108 <mypracticeemails@gmail.com>
2024-03-14 23:19:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/noteapp_roomdbmvvm/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.example.noteapp_roomdbmvvm
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.noteapp_roomdbmvvm.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+}
